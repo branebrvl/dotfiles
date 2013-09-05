db9cfd80984f16b59d69305317a696bc6429728d
branislav.vladisavljev <branebrvl@gmail.com>
2013-09-04 23:52:55 -0700

    updating setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,65 @@
 #!/bin/bash
-# Simple setup.sh
-ln -s dotfiles/.bash_profile
-ln -s dotfiles/.vim
-ln -s dotfiles/.oh-my-zsh
-ln -s dotfiles/.vimrc
-ln -s dotfiles/.zshrc
-ln -s dotfiles/.gitconfig
-ln -s dotfiles/bin/git_diff_wrapper bin/git_diff_wrapper
-echo "Initializing submodules..."
+
+export UNLINK=true
+
+function symlinkifne {
+    echo "WORKING ON: $1"
+    
+    # does it exist
+    if [[ -a $1 ]]; then
+      echo "  WARNING: $1 already exists."
+      
+      # If Unlink is requested
+      if [ "$UNLINK" = "true" ]; then
+          unlink $1
+          echo "  Unlinking $1"
+          
+          # create the link
+          export DOTLESS=`echo $1 | sed s/.//`
+          echo "  Symlinking $DOTFILESDIRRELATIVETOHOME/$DOTLESS to $1"
+          ln -s $DOTFILESDIRRELATIVETOHOME/$DOTLESS $1
+      else
+        echo "  SKIPPING $1."  
+      fi
+    # does not exist
+    else
+      # create the link
+      export DOTLESS=`echo $1 | sed s/.//`
+      echo "  Symlinking $DOTFILESDIRRELATIVETOHOME/$DOTLESS to $1"
+      ln -s $DOTFILESDIRRELATIVETOHOME/$DOTLESS $1
+    fi
+}
+
+
+echo "This script must be run from the dotfiles directory"
+echo "Setting up..."
+
+#export DOTFILESDIRRELATIVETOHOME=$PWD
+export DOTFILESDIRRELATIVETOHOME=dotfiles
+echo "DOTFILESDIRRELATIVETOHOME = $DOTFILESDIRRELATIVETOHOME"
+
+pushd ~
+
+symlinkifne .shellvars
+symlinkifne .shellpaths
+symlinkifne .shellaliases
+symlinkifne .profile
+symlinkifne .bash_profile
+symlinkifne .vim
+symlinkifne .oh-my-zsh
+symlinkifne .vimrc
+symlinkifne .zshrc
+symlinkifne .gitconfig
+
+cd dotfiles/
+
+echo "\n\n Initializing submodules..."
 git submodule init && git submodule update
+
+popd
+
+chsh -s `which zsh`
+/usr/bin/env zsh
+source ~/.zshrc
+
+echo "\n\n Dotfiles are now installed."
